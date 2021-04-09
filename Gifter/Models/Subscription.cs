2c27d637deca44d880a7ee3e7ca62753b72d1187
Rickie Le <rickienthefishies@gmail.com>
2021-04-09 14:30:08 -0500

    initial commit

---
+++ Gifter/Models/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public int SubscriberId { get; set; }
+        public int ProviderId { get; set; }
+        public UserProfile UserProfile { get; set; }
+    }
+}
